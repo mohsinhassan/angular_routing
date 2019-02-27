8dd9db71a0572134d8ecbb071c24f60aee6a29d4
mohsin hassan <mhassan@itdelivery.com.au>
2019-02-27 12:43:54 +0800

    on change react with setter and getter method

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input ,OnChanges, SimpleChanges} from '@angular/core';
+import { Component, OnInit , Input , SimpleChanges} from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,23 +6,34 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.scss']
 })
-export class DisplayEmployeeComponent implements OnInit , OnChanges {
- @Input() employee : Employee;
+export class DisplayEmployeeComponent implements OnInit  {
+  private _employee : Employee;
+  @Input()
+  set employee(val : Employee){
+    console.log("previous " + (this._employee ? this._employee.title : "null"));
+    console.log("current " + val.title);
+    this._employee = val;
+  }
+  get employee() : Employee
+  {
+    return this._employee;
+  }
+  
   constructor() { }
 
   ngOnInit() {
     console.log("here32");
   }
 
-  ngOnChanges( changes : SimpleChanges){
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
+  // ngOnChanges( changes : SimpleChanges){
+  //   const previousEmployee = <Employee>changes.employee.previousValue;
+  //   const currentEmployee = <Employee>changes.employee.currentValue;
 
 
-    console.log("previous : " + (previousEmployee ? previousEmployee.title : 'null'));
-    console.log("Current : " + (currentEmployee ? currentEmployee.title : 'null'));
-    //console.log(changes);
+  //   console.log("previous : " + (previousEmployee ? previousEmployee.title : 'null'));
+  //   console.log("Current : " + (currentEmployee ? currentEmployee.title : 'null'));
+  //   //console.log(changes);
 
-  } 
+  // } 
 
 }
