2c417686c74f8bbfc3d59ce0b3d2940eba923b28
mohsin hassan <mhassan@itdelivery.com.au>
2019-02-28 17:10:56 +0800

    next employee button added using observables

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { EmployeeService } from './services/employee.service';
 import { Employee } from '../models/employee.model';
 @Component({
@@ -8,14 +8,31 @@ import { Employee } from '../models/employee.model';
   styleUrls: ['./employee-details.component.scss']
 })
 export class EmployeeDetailsComponent implements OnInit {
-  private employee : Employee;
-  constructor(private _aRouter: ActivatedRoute , private _employeeService : EmployeeService) { }
+  private employee: Employee;
+  private _id : Number;
+  constructor(private _aRout: ActivatedRoute,
+    private _employeeService: EmployeeService,
+    private _router: Router) { }
 
 
   ngOnInit() {
-    const id = +this._aRouter.snapshot.paramMap.get('id');
-    this.employee = this._employeeService.getEmployee(id);
-    
+    this._aRout.paramMap.subscribe(params => {
+      this._id = +params.get('id');
+      console.log(params['id']);
+      this.employee = this._employeeService.getEmployee(this._id);
+    });
   }
 
+  viewNext()
+  {
+    if(this._id <2 ){
+      this._id =  + this._id + 1;
+    }
+    else{
+      this._id = 1;
+    }
+    
+
+    this._router.navigate(['/employees' , this._id]);
+  }
 }
